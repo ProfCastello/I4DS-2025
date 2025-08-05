20dbaaada2a1d0b923eab46e4699a15c60c360ca
ProfCastello <gustavo.castello@sp.senai.br>
2025-08-05 09:13:54 -0300

    feat: initialize ContadorApp with Expo, React, and React Native-dependencies Added package.json for handling project scripting and required libraries configuration. Including dependencies for Expo version, React version compatible for average"

---
+++ Programacao_Dispositivos_Moveis/ContadorApp/App.js
@@ -0,0 +1,82 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+export default function App() {
+  const [contador, setContador] = useState(0);
+
+  const handleIncrement = () => {
+    setContador(contador + 1);
+  };
+
+  const handleDecrement = () => {
+    contador > 0 && setContador(contador - 1);
+  };
+
+  const handleReset = () => {
+    setContador(0);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador</Text>
+      <Text style={styles.counterText}>{contador}</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={[styles.button, { marginRight: 20 }]}
+          onPress={handleDecrement}
+        >
+          <Text style={styles.buttonText}>Diminuir</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleIncrement}>
+          <Text style={styles.buttonText}>Aumentar</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity
+        style={[styles.button, styles.resetButton]}
+        onPress={handleReset}
+      >
+        <Text style={styles.buttonText}>Zerar</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  counterText: {
+    fontSize: 64,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: "#007bff",
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  resetButton: {
+    backgroundColor: "#dc3545",
+  },
+});
