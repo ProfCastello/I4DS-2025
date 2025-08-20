eab5b162c529fb74fab20f03956e7c1e531ef0e6
ProfCastello <gustavo.castello@sp.senai.br>
2025-08-20 08:29:04 -0300

    feat: initialize Temperature and Humidity monitoring app with MQTT integration
    - Added package.json with dependencies for React, Bootstrap, and MQTT.
    - Implemented main application structure in App.jsx.
    - Created components for displaying connection info, sensor data, and statistics.
    - Developed custom hooks for managing MQTT connections and theme context.
    - Styled the application with responsive design and dark/light themes.
    - Configured Vite for development and build processes.

--- Programacao_Front_End/01-Quiz-IoT/src/services/mqttService.js
+++ Programacao_Front_End/01-Quiz-IoT/src/services/mqttService.js
@@ -1,40 +1,39 @@
-import { Client } from 'paho-mqtt';
-
+import { Client } from "paho-mqtt";
 
 let client;
 
-
 export const connectMQTT = (onMessageReceived) => {
-// Usando wss na porta 8884
-client = new Client('broker.hivemq.com', 8884, '/mqtt', 'reactClient_' + Math.random());
+  // Usando wss na porta 8884
+  client = new Client(
+    "broker.hivemq.com",
+    8884,
+    "/mqtt",
+    "reactClient_" + Math.random() * 123
+  );
 
+  client.onConnectionLost = (responseObject) => {
+    console.log("Conexão perdida", responseObject);
+  };
 
-client.onConnectionLost = (responseObject) => {
-console.log('Conexão perdida', responseObject);
+  client.onMessageArrived = (message) => {
+    onMessageReceived(message.destinationName, message.payloadString);
+  };
+
+  client.connect({
+    useSSL: true, // habilita WSS
+    onSuccess: () => {
+      console.log("MQTT conectado via WSS!");
+      client.subscribe("quizIoT/resp_enviada");
+      client.subscribe("quizIoT/resultado");
+      client.subscribe("quizIoT/statusAluno");
+    },
+    onFailure: (err) => console.error("Erro MQTT", err),
+  });
 };
-
-
-client.onMessageArrived = (message) => {
-onMessageReceived(message.destinationName, message.payloadString);
-};
-
-
-client.connect({
-useSSL: true, // habilita WSS
-onSuccess: () => {
-console.log('MQTT conectado via WSS!');
-client.subscribe('quizIoT/resp_enviada');
-client.subscribe('quizIoT/resultado');
-client.subscribe('quizIoT/statusAluno');
-},
-onFailure: (err) => console.error('Erro MQTT', err),
-});
-};
-
 
 export const publishMessage = (topic, payload) => {
-if (!client) return;
-const message = new window.Paho.MQTT.Message(payload);
-message.destinationName = topic;
-client.send(message);
+  if (!client) return;
+  const message = new window.Paho.MQTT.Message(payload);
+  message.destinationName = topic;
+  client.send(message);
 };
